d37854d49fc68611104224004e6395ab9aa919cd
Niels Ringma <nielsringma@gmail.com>
2024-06-17 00:19:47 +0200

    Version check.

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.3.0"
+      version = ">=3.108.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = ">=1.0.0"
+      version = ">=1.13.0"
     }
   }
   cloud {
